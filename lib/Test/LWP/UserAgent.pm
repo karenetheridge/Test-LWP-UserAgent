96add6964614bf2b1eaace4a54f09f713d5f0ba3
Karen Etheridge <ether@cpan.org>
2015-07-26 10:25:17 -0700

    need nc 0.19 to get the version that uses non-deprecated Package::Stash interfaces

--- lib/Test/LWP/UserAgent.pm
+++ lib/Test/LWP/UserAgent.pm
@@ -18,7 +18,7 @@ use HTTP::Status qw(:constants status_message);
 use Try::Tiny;
 use Safe::Isa;
 use Carp;
-use namespace::clean -also => [qw(__isa_coderef __is_regexp)];
+use namespace::clean 0.19 -also => [qw(__isa_coderef __is_regexp)];
 
 my @response_map;
 my $network_fallback;
