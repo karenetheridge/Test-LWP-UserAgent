86e0b3a8606ab28a525d6b1dd2fe5eebe42c023d
Karen Etheridge <ether@cpan.org>
2016-10-03 19:25:12 -0700

    ensure vim modeline is in the first 5 line

--- lib/Test/LWP/UserAgent.pm
+++ lib/Test/LWP/UserAgent.pm
@@ -1,9 +1,9 @@
 use strict;
 use warnings;
 package Test::LWP::UserAgent;
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
 # ABSTRACT: A LWP::UserAgent suitable for simulating and testing network calls
 # KEYWORDS: testing useragent networking mock server client
-# vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.031';
 
