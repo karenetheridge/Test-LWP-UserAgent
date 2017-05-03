37891635084faef723db66689d995f00b7395812
Karen Etheridge <ether@cpan.org>
2017-05-03 15:57:42 -0700

    increment $VERSION after 0.032 release

--- lib/Test/LWP/UserAgent.pm
+++ lib/Test/LWP/UserAgent.pm
@@ -5,7 +5,7 @@ package Test::LWP::UserAgent;
 # ABSTRACT: A LWP::UserAgent suitable for simulating and testing network calls
 # KEYWORDS: testing useragent networking mock server client
 
-our $VERSION = '0.032';
+our $VERSION = '0.033';
 
 use parent 'LWP::UserAgent';
 use Scalar::Util qw(blessed reftype);
